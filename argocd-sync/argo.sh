567c8e14a4ba5d770c429c17e3f1e330b378c82c
Ugochi Ukaegbu <ugochiukaegbu19@gmail.com>
2024-03-07 11:33:11 +0100

    argocd script used to deploy the applications

---
+++ argocd-sync/argo.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+kubectl apply -f sock-shop-sync.yml
+kubectl apply -f routing-policy-sync.yml
+kubectl apply -f monitoring-sync.yml
+kubectl apply -f logging-sync.yml
+kubectl apply -f alerts-sync.yml
